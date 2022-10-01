14f2d4af7f698bef06f5414adcf2040e3f21d481
sebastian <scarullo@stacks.es>
2022-10-01 19:21:29 +0200

    Initial

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "=> 1.3.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.31.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
